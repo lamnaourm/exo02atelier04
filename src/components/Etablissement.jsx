6a9e289746fc3c3ec8a16c3eb54048a5d8fd1650
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-04 17:03:22 +0100

    solution exo02

---
+++ src/components/Etablissement.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Etablissement extends Component {
+    state = {
+        list_stgs : this.props.stagiaires
+    }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
