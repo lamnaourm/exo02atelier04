ac307946d2279ad5d78627b0da2d87e3a53ca3b4
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-04 17:14:17 +0100

    solution

--- src/components/Etablissement.jsx
+++ src/components/Etablissement.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
+import Stagiaire from './Stagiaire'
 
 export default class Etablissement extends Component {
     state = {
-        list_stgs : this.props.stagiaires
+        list_stgs: this.props.stagiaires
     }
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h1>Nombre de stagiaires : {this.state.list_stgs.length}</h1>
+                {
+                    this.state.list_stgs.map(s=> 
+                        <Stagiaire key={s.id} nom={s.nom} age={s.age} url={s.image} filiere={s.filiere} />
+                        )
+                }
+
+                <button onClick={() => this.setState({list_stgs: []})}>Clear ALL</button>
+                <button onClick={() => this.setState({list_stgs: this.props.stagiaires})}>ALL</button>
+            </div>
+        )
+    }
 }
--- src/components/Stagiaire.jsx
+++ src/components/Stagiaire.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Stagiaire({nom, age, filiere, image}) {
+export default function Stagiaire({nom, age, filiere, url}) {
   return (
     <div className='stagiaire'>
-        <img src={image} alt={nom} />
+        <img src={url} alt={nom} />
         <div>
             <h1>{nom} - {filiere}</h1>
             <h3>Age : {age}</h3>
